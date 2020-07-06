b33ffa50db9edb842800f110fb9fbaac1eee27d7
Owner <Owner@192.168.1.242>
2020-07-06 14:38:17 +0900

    finish challenge10

---
+++ challenge10/src/challenge10/Item.java
@@ -0,0 +1,22 @@
+package challenge10;
+
+public class Item {
+	// 商品の名前
+	String name;
+	//金額
+	int price;
+	//在庫
+	int stock;
+
+	//コンストラクタ（複数の情報を取り込んで設定）
+
+	//name 商品名
+	//price金額
+	//stock在庫
+	Item(String name, int price, int stock) {
+		this.name = name;
+		this.price = price;
+		this.stock = stock;
+
+	}
+}
--- challenge10/src/challenge10/Jidouhannbaiki.java
+++ challenge10/src/challenge10/Jidouhannbaiki.java
@@ -1,56 +1,95 @@
 package challenge10;
 
 public class Jidouhannbaiki {
-
+	/**
+	 * プログラムのメインメソッド。ここから始まる。
+	 * @param args　起動時引数
+	 */
 	public static void main(String[] args) {
 
-		//入力した数値input
 		java.util.Scanner input = new java.util.Scanner(System.in);
-		//商品
-		String name = "コーヒー";
-		int price = 150;
-		String name1 = "お茶";
-		int price1 = 110;
-		String name2 = "炭酸飲料";
-		int price2 = 120;
-		//合計金額を覚えておく変数の準備。初期値は０円に設定。
+		//商品情報を覚えておくところ（変数）の準備
+		Item[] items = prepareItems();
+		//購入金額　初期値は0。
 		int total = 0;
-		//繰り返し 14章
+		//繰り返し
 		do {
-			//商品一覧を表示する
-			System.out.println("1." + name);
-			System.out.println("2." + name1);
-			System.out.println("3." + name2);
+			//商品一覧を表示
+			showMenu(items);
 			System.out.println("-------------");
-
-			//選ぶやつも14章のselect
+			//入力値（商品番号:select）
 			int select = input.nextInt();
-			//もし99の値を入れたら終了する
+
+			//99終了
 			if (select == 99) {
-
+				//在庫を表示させる
+				showZaiko(items);
+				System.out.println();
 				break;
+				//普通の入力値（1-3）
+			} else {
+				select = select - 1;
 			}
-			//商品が売れる
-			switch (select) {
-			case 1: {
-				//売上計算。売上合計変数:totalに今回の売上をプラスして更新させます。
-				total = total + price;
+			//番号の商品販売
+			if (!canSell(items, select)) {
+				continue;
 			}
-				break;
-			case 2: {
-
-				total = total + price1;
-			}
-				break;
-			case 3: {
-
-				total = total + price2;
-			}
-			}
+			//販売したら売上計上
+			total += sell(items, select);
+			//在庫が１個減る
+			items[select].stock--;
+			//商品が売り切れているかをチェック
 
 		} while (true);
-		//売上
-		System.out.println("売上:" + total);
+		//自動販売機、終了
+		showEndMessage(total, items);
+	}
+
+	public static void showEndMessage(int total, Item[] a) {
+		//売上表示
+		System.out.println("売上:" + total + "円");
+	}
+
+	public static int sell(Item[] items, int select) {
+		//商品が売れる、売り上げが増える
+		System.out.println();
+		//売り上げの計算totalに計上
+		return items[select].price;
+	}
+
+	public static boolean canSell(Item[] items, int select) {
+
+		if (items[select].stock > 0) {
+
+		}
+		return true;
+	}
+
+	//a は、商品一覧のデータが入っている配列
+
+	public static void showMenu(Item[] a) {
+		//商品一覧を表示
+		for (int i = 0; i < a.length; i++) {
+			//iをどうすればi+1にできるか？その場合totalにも＋１表記が必要 ※そもそもselectから1引けば1は0になる
+			if (a[i].stock > 0) {
+				System.out.println(a[i].name);//+ a[i].stock + "本"
+			//商品が0より大きい場合のみ表示する
+			}
+		}
+	}
+	public static void showZaiko(Item[]a) {
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i].name+ a[i].stock + "本");
+		}
+	}
+	public static Item[] prepareItems() {
+		// 商品の準備(商品名,価格,在庫数)
+		Item[] a = new Item[] {
+				new Item("1. コーヒー", 150, 5),
+				new Item("2. お茶", 110, 5),
+				new Item("3. 炭酸水", 120, 5),
+		};
+		return a;
 	}
 
 }
