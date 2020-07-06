42ea376eade3d67359752965cf1600b28202e393
Owner <Owner@192.168.1.242>
2020-07-06 16:27:22 +0900

    0706

--- challenge10/src/challenge10/Jidouhannbaiki.java
+++ challenge10/src/challenge10/Jidouhannbaiki.java
@@ -1,12 +1,7 @@
 package challenge10;
 
 public class Jidouhannbaiki {
-	/**
-	 * プログラムのメインメソッド。ここから始まる。
-	 * @param args　起動時引数
-	 */
 	public static void main(String[] args) {
-
 		java.util.Scanner input = new java.util.Scanner(System.in);
 		//商品情報を覚えておくところ（変数）の準備
 		Item[] items = prepareItems();
@@ -23,13 +18,14 @@ public class Jidouhannbaiki {
 			//99終了
 			if (select == 99) {
 				//在庫を表示させる
-				showZaiko(items);
+				setShowZaiko(items);
 				System.out.println();
 				break;
 				//普通の入力値（1-3）
 			} else {
 				select = select - 1;
 			}
+
 			//番号の商品販売
 			if (!canSell(items, select)) {
 				continue;
@@ -45,20 +41,21 @@ public class Jidouhannbaiki {
 		showEndMessage(total, items);
 	}
 
-	public static void showEndMessage(int total, Item[] a) {
+	private static void showEndMessage(int total, Item[] a) {
 		//売上表示
 		System.out.println("売上:" + total + "円");
 	}
 
-	public static int sell(Item[] items, int select) {
+	private static int sell(Item[] items, int select) {
 		//商品が売れる、売り上げが増える
 		System.out.println();
 		//売り上げの計算totalに計上
 		return items[select].price;
 	}
 
-	public static boolean canSell(Item[] items, int select) {
-
+	//売れる場合にはCanSellにて計上していく
+	private static boolean canSell(Item[] items, int select) {
+		//在庫がある状況
 		if (items[select].stock > 0) {
 
 		}
@@ -67,22 +64,25 @@ public class Jidouhannbaiki {
 
 	//a は、商品一覧のデータが入っている配列
 
-	public static void showMenu(Item[] a) {
+	private static void showMenu(Item[] a) {
 		//商品一覧を表示
 		for (int i = 0; i < a.length; i++) {
 			//iをどうすればi+1にできるか？その場合totalにも＋１表記が必要 ※そもそもselectから1引けば1は0になる
 			if (a[i].stock > 0) {
 				System.out.println(a[i].name);//+ a[i].stock + "本"
-			//商品が0より大きい場合のみ表示する
+				//商品が0より大きい場合のみ表示する
 			}
 		}
 	}
-	public static void showZaiko(Item[]a) {
+
+	//在庫一覧を表示
+	 public static void setShowZaiko(Item[] a) {
 		for (int i = 0; i < a.length; i++) {
-			System.out.println(a[i].name+ a[i].stock + "本");
+			System.out.println(a[i].name + a[i].stock + "本");
 		}
 	}
-	public static Item[] prepareItems() {
+
+	private static Item[] prepareItems() {
 		// 商品の準備(商品名,価格,在庫数)
 		Item[] a = new Item[] {
 				new Item("1. コーヒー", 150, 5),
